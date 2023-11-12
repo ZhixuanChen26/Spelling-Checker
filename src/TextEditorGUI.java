5449e204d89ef28f3ac232b57d5ac03d1d091bed
Yi Ran <yran8@uwo.ca>
2023-11-12 15:40:53 -0500

    GUI

---
+++ src/TextEditorGUI.java
@@ -0,0 +1,100 @@
+import javax.swing.*;
+import javax.swing.text.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.nio.file.*;
+import java.util.List;
+public class TextEditorGUI {
+    private JFrame frame;
+    private JTextPane textPane;
+    private JButton OpenButton, CheckButton, ExitButton;
+    private JScrollPane scrollPane;
+    private JFileChooser fileChooser;
+    private SysDictionary legalDic;
+
+    public TextEditorGUI() {
+        legalDic = new SysDictionary("words_alpha.txt");
+
+        frame = new JFrame("Text Editor");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 400);
+
+        textPane = new JTextPane();
+        scrollPane = new JScrollPane(textPane);
+
+        OpenButton = new JButton("File Selector");
+        CheckButton = new JButton("Check");
+        ExitButton = new JButton("Exit");
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(OpenButton);
+        buttonPanel.add(CheckButton);
+        buttonPanel.add(ExitButton);
+
+        frame.add(buttonPanel, BorderLayout.NORTH);
+        frame.add(scrollPane, BorderLayout.CENTER);
+
+        fileChooser = new JFileChooser();
+
+        OpenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int returnValue = fileChooser.showOpenDialog(frame);
+                if (returnValue == JFileChooser.APPROVE_OPTION) {
+                    Path selectedFile = fileChooser.getSelectedFile().toPath();
+                    try {
+                        String content = new String(Files.readAllBytes(selectedFile));
+                        textPane.setText(content);
+                    } catch (Exception exp) {
+                        exp.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        CheckButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                highlightMisspelledWords();
+            }
+        });
+
+        ExitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+            }
+        });
+
+    }
+
+    private void highlightMisspelledWords() {
+        // Set the style that will highlight the wrong words later
+        StyledDocument doc = textPane.getStyledDocument();
+        Style style = doc.addStyle("RedUnderline", null);
+        StyleConstants.setUnderline(style, true);
+        StyleConstants.setForeground(style, Color.RED);
+
+        String text = textPane.getText();
+        int idx = 0;  // Use index to keep track of current processing location of the text
+        for (String word : text.split("\\W+")) {
+            if (!legalDic.hasWord(word.toLowerCase())) {
+                int length = word.length();
+                doc.setCharacterAttributes(idx, length, style, false);
+            }
+            idx += word.length();
+            // Skipping non-word chars till the start of next word
+            while (idx < text.length() && !Character.isLetter(text.charAt(idx))) idx++;
+        }
+    }
+
+    public void display() {
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new TextEditorGUI().display();
+        });
+    }
+}
