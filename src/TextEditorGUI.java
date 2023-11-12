9210e92bab11cd10fcd746239cfeedf7f6f49e10
Yi Ran <yran8@uwo.ca>
2023-11-12 16:48:47 -0500

    test reset window size

--- src/TextEditorGUI.java
+++ src/TextEditorGUI.java
@@ -17,7 +17,7 @@ public class TextEditorGUI {
 
         frame = new JFrame("Text Editor");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 400);
+        frame.setSize(1000, 800);
 
         textPane = new JTextPane();
         scrollPane = new JScrollPane(textPane);
