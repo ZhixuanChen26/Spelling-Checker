7f6de34b697c2dced6c0a46159cd42f38103dbba
Sizr <sizirui1999@gmail.com>
2023-11-25 19:11:38 -0500

    update the implementation of several functions

--- src/TextEditorGUI.java
+++ src/TextEditorGUI.java
@@ -8,6 +8,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 public class TextEditorGUI {
     private JFrame frame;
@@ -18,8 +19,17 @@ public class TextEditorGUI {
     private SysDictionary legalDic;
     private UserDictionary userDic = new UserDictionary();
     private File currentFile;
+    private List<String> ignoredWords = new ArrayList<>();
+
+    private static final String IGNORE_ONCE_ATTRIBUTE = "ignoreOnce";
+
+
 
     public TextEditorGUI() {
+
+        /*
+        Sizr
+         */
         legalDic = new SysDictionary("words_alpha.txt");
 
         frame = new JFrame("Text Editor");
@@ -49,6 +59,8 @@ public class TextEditorGUI {
             @Override
             public void actionPerformed(ActionEvent e) {
                 int returnValue = fileChooser.showOpenDialog(frame);
+                //我们在这里把arraylist重新进行更新
+                ignoredWords = new ArrayList<>();
                 if (returnValue == JFileChooser.APPROVE_OPTION) {
                     Path selectedFile = fileChooser.getSelectedFile().toPath();
                     currentFile = fileChooser.getSelectedFile();
@@ -137,7 +149,86 @@ public class TextEditorGUI {
                             userDic.add(selectWord);
                         });
                         parentMenu.add(addToDicItem);
+/*
+Sizr
+ */
 
+                        JMenuItem deleteItem = new JMenuItem("Delete");
+                        deleteItem.addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent ae) {
+                                try {
+                                    int offset = textPane.viewToModel2D(e.getPoint());
+                                    int wordStart = Utilities.getWordStart(textPane, offset);
+                                    int wordEnd = Utilities.getWordEnd(textPane, offset);
+                                    if (wordEnd < textPane.getDocument().getLength() && textPane.getText(wordEnd, 1).equals(" ")) {
+                                        wordEnd++; // 如果后面有空格，则将结束位置向后移动一个字符
+                                    } else if (wordStart > 0 && textPane.getText(wordStart - 1, 1).equals(" ")) {
+                                        wordStart--; // 如果后面没有空格，但前面有空格，则将开始位置向前移动一个字符
+                                    }
+
+                                    textPane.getDocument().remove(wordStart, wordEnd - wordStart);
+                                    highlightMisspelledWords(); // 重新进行拼写检查
+                                } catch (BadLocationException ex) {
+                                    ex.printStackTrace();
+                                }
+                            }
+                        });
+                        parentMenu.add(deleteItem);
+
+                        JMenuItem ignoreAllItem = new JMenuItem("Ignore All");
+                        ignoreAllItem.addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent ae) {
+                                try {
+                                    int offset = textPane.viewToModel2D(e.getPoint());
+                                    int wordStart = Utilities.getWordStart(textPane, offset);
+                                    int wordEnd = Utilities.getWordEnd(textPane, offset);
+                                    String selectedWord = textPane.getDocument().getText(wordStart, wordEnd - wordStart);
+                                    ignoredWords.add(selectedWord.toLowerCase());
+                                    highlightMisspelledWords(); // 重新进行拼写检查，忽略选中的单词
+                                } catch (BadLocationException ex) {
+                                    ex.printStackTrace();
+                                }
+                            }
+                        });
+                        parentMenu.add(ignoreAllItem);
+
+                        JMenuItem ignoreOnceItem = new JMenuItem("Ignore Once");
+                        ignoreOnceItem.addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent ae) {
+                                try {
+                                    int offset = textPane.viewToModel2D(e.getPoint());
+                                    int wordStart = Utilities.getWordStart(textPane, offset);
+                                    int wordEnd = Utilities.getWordEnd(textPane, offset);
+                                    String selectedWord = textPane.getDocument().getText(wordStart, wordEnd - wordStart);
+
+
+                                    // 先删除选中的单词
+                                    textPane.getDocument().remove(wordStart, wordEnd - wordStart);
+
+                                    // 重新插入该单词，并应用特殊样式
+                                    Style ignoreStyle = textPane.addStyle(IGNORE_ONCE_ATTRIBUTE, null);
+                                    StyleConstants.setForeground(ignoreStyle, Color.darkGray);
+                                    StyleConstants.setUnderline(ignoreStyle, false);
+                                    textPane.getDocument().insertString(wordStart, selectedWord, ignoreStyle);
+
+
+                                    // 重新进行拼写检查
+                                    highlightMisspelledWords();
+                                } catch (BadLocationException ex) {
+                                    ex.printStackTrace();
+                                }
+                            }
+                        });
+
+                        parentMenu.add(ignoreOnceItem);
+
+
+/*
+Sizr
+ */
                         // Show the context menu
                         parentMenu.show(textPane, e.getX(), e.getY());
                     } catch (BadLocationException ex) {
@@ -151,12 +242,16 @@ public class TextEditorGUI {
     private void highlightMisspelledWords() {
         // Retrieve the document from the text pane for styling
         StyledDocument doc = textPane.getStyledDocument();
+        String text = textPane.getText();
 
-        // Define a normal style to un-highlight the text after correction
-        Style normalStl = textPane.addStyle("NormalStyle", null);
-        StyleConstants.setForeground(normalStl, Color.BLACK);
-        StyleConstants.setUnderline(normalStl, false);
-        doc.setCharacterAttributes(0, doc.getLength(), normalStl, false);
+
+
+        // 重置整个文档的样式为 normalStyle
+        Style normalStyle = textPane.addStyle("NormalStyle", null);
+        StyleConstants.setForeground(normalStyle, Color.BLACK);
+        StyleConstants.setUnderline(normalStyle, false);
+        doc.setCharacterAttributes(0, doc.getLength(), normalStyle, true);
+
 
         // Define a red underline style for spelling errors
         Style redStyle = doc.addStyle("RedUnderline", null);
@@ -173,8 +268,9 @@ public class TextEditorGUI {
         StyleConstants.setUnderline(greenStyle, true);
         StyleConstants.setForeground(greenStyle, Color.GREEN);
 
-        // Get the entire text from the text pane
-        String text = textPane.getText();
+//Sizr
+        doc.setCharacterAttributes(0, doc.getLength(), normalStyle, true);
+
         // Index to keep track of the current location in the text
         int idx = 0;
         // Flags to track sentence boundaries
@@ -185,38 +281,42 @@ public class TextEditorGUI {
 
         // Split the text into words based on non-word characters
         for (String word : text.split("\\W+")) {
-            // Check if the word contains only alphabetic characters
-            if (word.matches("^[a-zA-Z]+$")) {
-                // Determine if the word is mixed case or improperly capitalized
-                boolean isMixedCase = !word.equals(word.toLowerCase())
-                        && !word.equals(word.toUpperCase())
-                        && !(Character.isUpperCase(word.charAt(0))
-                        && word.substring(1).equals(word.substring(1).toLowerCase()));
-
-                // Check if the word should be capitalized (start of a new sentence)
-                boolean isStartOfSentenceError = isNewSentence && Character.isLowerCase(word.charAt(0));
-
-                // Check if the word is spelled correctly. The order of how we highlight the different errors matters.
-                if (!legalDic.hasWord(word.toLowerCase()) && !userDic.hasWord(word)) {
-                    int length = word.length();
-                    doc.setCharacterAttributes(idx, length, redStyle, false);
-                }
-
-                // Apply blue underline style for capitalization errors
-                if (isMixedCase || isStartOfSentenceError) {
-                    int length = word.length();
-                    doc.setCharacterAttributes(idx, length, blueStyle, false);
-                }
-
-                // Highlight double words in green
-                if (word.equalsIgnoreCase(prev) && !isDiffSentence) {
-                    int length = word.length();
-                    doc.setCharacterAttributes(idx, length, greenStyle, false);
-                }
-            }
-
-            // Calculate the end index of the current word
+            int wordStart = idx;
             int wordEnd = idx + word.length();
+
+            // Check if the word contains only alphabetic characters
+
+            if (word.matches("^[a-zA-Z]+$") && !ignoredWords.contains(word.toLowerCase())) {
+                    // Determine if the word is mixed case or improperly capitalized
+                    boolean isMixedCase = !word.equals(word.toLowerCase())
+                            && !word.equals(word.toUpperCase())
+                            && !(Character.isUpperCase(word.charAt(0))
+                            && word.substring(1).equals(word.substring(1).toLowerCase()));
+
+                    // Check if the word should be capitalized (start of a new sentence)
+                    boolean isStartOfSentenceError = isNewSentence && Character.isLowerCase(word.charAt(0));
+
+                    // Check if the word is spelled correctly. The order of how we highlight the different errors matters.
+                    if (!legalDic.hasWord(word.toLowerCase()) && !userDic.hasWord(word)) {
+                        int length = word.length();
+                        doc.setCharacterAttributes(idx, length, redStyle, false);
+                    }
+
+                    // Apply blue underline style for capitalization errors
+                    if (isMixedCase || isStartOfSentenceError) {
+                        int length = word.length();
+                        doc.setCharacterAttributes(idx, length, blueStyle, false);
+                    }
+
+                    // Highlight double words in green
+                    if (word.equalsIgnoreCase(prev) && !isDiffSentence) {
+                        int length = word.length();
+                        doc.setCharacterAttributes(idx, length, greenStyle, false);
+                    }
+                }
+
+
+
             // Update idx to the position of the next word
             idx += word.length();
 
