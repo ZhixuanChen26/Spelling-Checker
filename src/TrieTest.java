a79d9dfc689332a0d8e4ba8751094534e077e9fb
Jingwen Liu <jliu2768@uwo.ca>
2023-11-30 19:32:39 +0000

    TrieTest.java edited online with Bitbucket

--- src/TrieTest.java
+++ src/TrieTest.java
@@ -5,7 +5,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TrieTest {
-   /**
+    /**
      * Test the insert and contains methods of the Trie.
      * This test first ensures that the Trie does not contain a specific word ('apple') before it's inserted. 
      * After insertion, it checks if the Trie correctly reports that it now contains the word.
@@ -18,7 +18,7 @@ class TrieTest {
         trie.insert("apple");
         assertTrue(trie.contains("apple"), "Trie should contain 'apple' after insertion");
     }
-   /**
+    /**
      * Test the suggestCorrections method of the Trie.
      * This test inserts a couple of words ('apple' and 'app') into the Trie and then asks for corrections for a typo ('aple'). 
      * It checks if the returned suggestionscorrectly include the words 'apple' and 'app'.
