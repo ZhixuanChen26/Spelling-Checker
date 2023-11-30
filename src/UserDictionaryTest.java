5ddc6888d84a13d120a3097c475798fe3f977ad8
Zhixuan Chen <zche453@uwo.ca>
2023-11-30 21:35:43 +0000

    Update javadoc

--- src/UserDictionaryTest.java
+++ src/UserDictionaryTest.java
@@ -2,8 +2,19 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class UserDictionaryTest {
+/**
+ * Used to test UserDictionary.java
+ * @author Zhixuan Chen
+ * @version 1.0
+ * @since 2023-11-22
+ */
 
+class UserDictionaryTest {
+     /**
+     * Tests the addAndHasWord method of UserDictionary.
+     * It first checks that a new word is not already in the dictionary,
+     * then adds the word and checks if the dictionary reflects this addition.
+     */
     @Test
     void addAndHasWord() {
         UserDictionary userDict = new UserDictionary(); // Make sure the file exists or handle the case where it doesn't
@@ -14,7 +25,11 @@ class UserDictionaryTest {
         userDict.add(testWord);
         assertTrue(userDict.hasWord(testWord), "Dictionary should contain the word after adding");
     }
-
+  
+    /**
+     * Tests the removeWord method of UserDictionary.
+     * It adds a word, checks its presence, then removes it and verifies its absence.
+     */
     @Test
     void removeWord() {
         UserDictionary userDict = new UserDictionary(); // Make sure the file exists or handle the case where it doesn't
@@ -27,6 +42,10 @@ class UserDictionaryTest {
         assertFalse(userDict.hasWord(testWord), "Dictionary should not contain the word after removal");
     }
 
+    /**
+     * Tests the hasWord method of UserDictionary.
+     * It adds a word, verifies its presence, and also checks for a non-existent word.
+     */
     @Test
     void hasWord() {
         UserDictionary userDict = new UserDictionary(); // Make sure the file exists or handle the case where it doesn't
